f73d4c856dc0b481c208c73366dccd61018fa07e
ioioi <1146040444@qq.com>
2022-10-31 12:28:13 +0800

    update sse

--- w/ssecode.js
+++ w/ssecode.js
@@ -3,19 +3,36 @@
 function makeSSE (name) {
 
   return `<script>
-    let es = new EventSource(__prepath__ + '/sse')
+    ;(() => {
+      let esTimer = null;
+      let updateTime = 0;
+      let es = new EventSource(__prepath__ + '/sse')
 
-    es.onerror = function (err) {
+      es.onerror = function (err) {
 
-    }
+      }
 
-    es.addEventListener('message', e => {
-    
-    })
+      es.addEventListener('message', e => {
+      
+      })
 
-    es.addEventListener('update', e => {
-      location.reload(true)
-    })
+      es.addEventListener('update', e => {
+        updateTime = w.storage.get('app_reload_time') || 0
+        let tm = Date.now()
+        if (tm - parseInt(updateTime) > 2000) {
+          w.storage.set('app_reload_time', tm)
+          location.reload(true)
+        } else {
+          if (esTimer) clearTimerout(esTimer)
+          
+          esTimer = setTimeout(() => {
+            esTimer = null
+            w.storage.set('app_reload_time', tm)
+            location.reload(true)
+          }, 2000)
+        }
+      })
+    })();
 
   </script>`;
 
