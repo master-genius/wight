acbda74b6fad7fb1183d7773b1f1b3042751fc96
wang <1146040444@qq.com>
2023-04-04 18:37:27 +0800

    --

--- w/newproject/app.js
+++ w/newproject/app.js
@@ -5,6 +5,26 @@ const pushStart = require('pushStart')
 
 pushStart()
 
+let allowPages = [
+  'login'
+]
+
+if (!w.dev) {
+  w.host = location.protocol + '//' + location.host
+}
+
+/*
+w.addHook((c) => {
+  let r = checkToken(true)
+  if (!r && allowPages.indexOf(c.path) < 0) {
+    acover('<user-login></user-login>')
+  }
+
+  return r
+}, {name: 'check-token', exclude: []})
+*/
+
+
 //测试代码，在app.js文件中你可以直接在顶层使用await。
 notice('正在加载···')
 
