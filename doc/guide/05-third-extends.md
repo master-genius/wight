b85c4227bee768df8a6fda577f9b98257e6c4ad4
ioioi <1146040444@qq.com>
2023-10-23 12:55:51 +0800

    --

---
+++ doc/guide/05-third-extends.md
@@ -0,0 +1,19 @@
+<!--name:内置扩展和第三方扩展-->
+# 内置扩展和第三方扩展
+
+框架层面在创建项目的时候，会默认提供一些扩展，这些扩展为开发工作提供了巨大的方便：
+
+| 扩展文件 | 说明 | 详细 |
+| ---- | ---- | ---- |
+| pushStart.js | 自动把#添加到第一条历史记录 |  |
+| confirm.js | 重写了window.confirm | 传递选项用于描述提示文本，以及确认的回调函数和取消的回调函数 |
+| apicall.js | 封装fetch提供的api调用扩展 | 自动检测authorization，自动执行成功或失败的回调，提供get、post、put、delete常用方法。 |
+| token.js | token管理扩展 |  |
+| checkToken.js | 检测是否登录 |  |
+| userinfo.js | 管理登录用户的基本信息 |  |
+| ejson.js | 对object进行encodeURIComponent(JSON.stringify(data))处理 |  |
+| htmltag.js | 用于模板字符串的格式化处理 | 自动进行各个插值数据的检测，并对非标准的html文本转码，使用htmltag.ehtml可以直接转码带标签的数据，htmltag默认是会检测插入的文本是不是合法的html文本，如果是合法的则不会转码 &lt; 和 &gt; |
+| storageEvent.js | 监听storage变化并触发对应的函数 | 此扩展可以实现一个浏览器打开多个页面，页面之前的状态同步，比如一个页面退出了，另一个页面会检测到，并且也可以跳转到登录页。 |
+| timestr.js | 格式化时间字符串 | 格式：2023-10-23_23-15-06 |
+
+这些扩展其实就是按照扩展的规则编写的，但是从一开始就内置到项目上，开发者根据需求选择是否启用。
--- w/newproject/extends/timestr.js
+++ w/newproject/extends/timestr.js
@@ -1,14 +1,31 @@
-exports.timestr = function (t = null) {
+
+let fmtbits = (n) => {
+  return n < 10 ? `0${n}` : n;
+}
+
+module.exports = function (mode='long', t=null) {
+  if (typeof mode === 'number' || mode && mode instanceof Date) {
+    t = mode;
+    mode = 'long';
+  }
+
   if (t === null) {
     t = Date.now();
   } else if (typeof t === 'string') {
     t = parseInt(t);
   }
 
-  let tm = new Date(t);
+  let tm = typeof t === 'number' ? new Date(t) : t;
 
-  let tmstr = `${tm.getFullYear()}-${tm.getMonth()+1}-${tm.getDate()} `
-          +`${tm.getHours()}:${tm.getMinutes()}:${tm.getSeconds()}`;
+  let tmstr = `${tm.getFullYear()}-${fmtbits(tm.getMonth()+1)}-${fmtbits(tm.getDate())}`;
+
+  if (mode === 'short') {
+    return tmstr; 
+  }
+
+  if (mode === 'middle') {
+    return tmstr + '_' + `${fmtbits(tm.getHours())}-${fmtbits(tm.getMinutes())}`;
+  }
   
-  return tmstr;
+  return tmstr + `_${fmtbits(tm.getHours())}-${fmtbits(tm.getMinutes())}-${fmtbits(tm.getSeconds())}`;
 };
