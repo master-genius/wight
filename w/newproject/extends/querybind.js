9d211ed4bdc3c14cb8ee69217f44a710861a1036
wang <1146040444@qq.com>
2024-03-27 14:13:28 +0800

    2024年 03月 27日 星期三 14:13:28 CST

--- w/newproject/extends/querybind.js
+++ w/newproject/extends/querybind.js
@@ -1,17 +1,24 @@
+function query(qss,callback=null) {
+  let nod = this.querySelector(qss);
+  if (!nod) return null;
+  if (callback && typeof callback === 'function') callback(nod);
+  return nod;
+}
+
+function queryAll(qss, callback=null) {
+  let nds = this.querySelectorAll(qss);
+  if (callback && typeof callback === 'function') nds.forEach(callback);
+  return nds;
+}
 
 if (window.Element && typeof window.Element === 'function' && window.Element.prototype) {
-  window.Element.prototype.query = function query (qss,callback=null) {
-    let nod = this.querySelector(qss);
-    if (!nod) return null;
-    if (callback && typeof callback === 'function') callback(nod);
-    return nod;
-  }
+  window.Element.prototype.query = query;
+  window.Element.prototype.queryAll = queryAll;
+}
 
-  window.Element.prototype.queryAll =  function (qss, callback=null) {
-    let nds = this.querySelectorAll(qss);
-    if (callback && typeof callback === 'function') nds.forEach(callback);
-    return nds;
-  }
+if (window.DocumentFragment && typeof window.DocumentFragment === 'function' && window.DocumentFragment.prototype) {
+  window.DocumentFragment.prototype.query = query;
+  window.DocumentFragment.prototype.queryAll = queryAll;
 }
 
 module.exports = {}
