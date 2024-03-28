4dcb7c6de28ba2d47fedfbc5405667eaf246c963
wang <1146040444@qq.com>
2024-03-28 10:48:42 +0800

    2024年 03月 28日 星期四 10:48:42 CST

--- w/newproject/extends/renders.js
+++ w/newproject/extends/renders.js
@@ -1,14 +1,38 @@
 'use strict'
 
-Array.prototype.renders = function (callback, firstText='', endText='') {
-  if (!callback || typeof callback !== 'function') {
-    w.debug && w.notifyTopError(`renderHTML: callback必须是一个函数`)
-    return '';
-  }
+Object.defineProperty(Array.prototype, 'renders', {
+  value: function (callback, firstText='', endText='') {
+    if (!callback || typeof callback !== 'function') {
+      w.debug && w.notifyTopError(`renderHTML: callback必须是一个函数`)
+      return '';
+    }
+    
+    let htmls = this.map(callback);
   
-  let htmls = this.map(callback);
+    return `${firstText}${htmls.join('')}${endText}`;
+  },
+  enumerable: false,
+  writable: false,
+  configurable: false
+});
 
-  return `${firstText}${htmls.join('')}${endText}`;
-}
+Object.defineProperty(Object.prototype, 'renders', {
+  value: function (callback, firstText='', endText='') {
+    if (!callback || typeof callback !== 'function') {
+      w.debug && w.notifyTopError(`renderHTML: callback必须是一个函数`)
+      return '';
+    }
+    
+    let htmls = [];
+    for (let k in this) {
+      htmls.push(callback(k, this[k]) || '');
+    }
+  
+    return `${firstText}${htmls.join('')}${endText}`;
+  },
+  enumerable: false,
+  writable: false,
+  configurable: false
+});
 
 module.exports = {}
